b15ee9a8a2e0f167c73229d5f7a5f3af7a8b4dfe
ruofei <rf.wangchn@gmail.com>
2020-02-23 01:30:08 +0800

    Support: insert images to your page.

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-var url = ''
+var url = '';
 
 module.exports = {
   blocks: {
     nce: {
       process: function(block) {
+        var type = block.kwargs.type;
         var src = block.kwargs.src;
   
         if (src.includes('https://') && !block.kwargs.book) {
           url = src;
         } else {
-          url = `https://git.lug.ustc.edu.cn/smpower/nce${block.kwargs.book}-aep/raw/master/${src}/`
+          if (type === 'audio') {
+            url = `https://git.lug.ustc.edu.cn/smpower/nce${block.kwargs.book}-aep/raw/master/${src}/`;
+          } else if (type === 'image') {
+            url = `https://git.lug.ustc.edu.cn/smpower/nce${block.kwargs.book}-img/raw/master/${src}/`;
+          } else {
+            url = '';
+          }
         }
 
-        var audioStr = this.output.name === 'website' ? `
-          <audio controls="controls">
-            <source src=${url} type="audio/mpeg" />
-            <i>Your browser does not support the audio element.</i>
-          </audio>
-        ` : null
+        var elementStr = this.output.name === 'website' ?
+          type === 'audio' ?
+            `
+              <audio controls="controls">
+                <source src=${url} type="audio/mpeg" />
+                <i>Your browser does not support the audio element.</i>
+              </audio>
+            `
+          : type === 'image' ? `<img src=${url} />` : null
+        : null;
 
-        return `
-          <div className="NceBlock">
-            ${audioStr}
-          </div>
-        `
+        return `<div className="NceBlock">${elementStr}</div>`;
       }
     }
   }
