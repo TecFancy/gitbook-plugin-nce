c2f75fc9ca18400b7af85724cec0d5233c3d126a
ruofei <rf.wangchn@gmail.com>
2020-02-23 02:33:56 +0800

    Fixed problem: image block and audio block.

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-var url = '';
+
+// Get url of audio or image.
+function getUrl(block, type) {
+  var url = '';
+  var book = undefined;
+  var src = block.kwargs.src;
+
+  if (block.kwargs.book) book = block.kwargs.book;
+  else book = undefined;
+
+  if (src.includes('https://') && !book) {
+    url = src;
+  } else {
+    if (type === 'audio') {
+      url = `https://git.lug.ustc.edu.cn/smpower/nce${book}-aep/raw/master/${src}/`;
+    } else if (type === 'image') {
+      url = `https://git.lug.ustc.edu.cn/smpower/nce${book}-img/raw/master/${src}/`;
+    } else return '';
+  }
+
+  return url;
+}
 
 module.exports = {
   blocks: {
-    nce: {
+    // audio block
+    nceAudio: {
       process: function(block) {
-        var type = block.kwargs.type;
-        var src = block.kwargs.src;
-  
-        if (src.includes('https://') && !block.kwargs.book) {
-          url = src;
-        } else {
-          if (type === 'audio') {
-            url = `https://git.lug.ustc.edu.cn/smpower/nce${block.kwargs.book}-aep/raw/master/${src}/`;
-          } else if (type === 'image') {
-            url = `https://git.lug.ustc.edu.cn/smpower/nce${block.kwargs.book}-img/raw/master/${src}/`;
-          } else {
-            url = '';
-          }
-        }
-
-        var className = type === 'audio' ? 'NceBlock audio' : type === 'image' ? 'NceBlock image' : 'NceBlock';
-
         var elementStr = this.output.name === 'website' ?
-          type === 'audio' ?
-            `
-              <audio controls="controls">
-                <source src=${url} type="audio/mpeg" />
-                <i>Your browser does not support the audio element.</i>
-              </audio>
-            `
-          : type === 'image' ? `<img src=${url} />` : null
+          `
+            <audio controls="controls">
+              <source src=${getUrl(block, 'audio')} type="audio/mpeg" />
+              <i>Your browser does not support the audio element.</i>
+            </audio>
+          `
         : null;
 
-        return `<div class=${className}>${elementStr}</div>`
+        return `<div class="NceBlock audio">${elementStr}</div>`;
       }
+    },
+
+    // image block
+    nceImage: function(block) {
+      var elementStr = this.output.name === 'website' 
+        ? `<img src=${getUrl(block, 'image')} alt="Content image" />` 
+        : null;
+
+      return `<div class="NceBlock image">${elementStr}</div>`;
     }
   }
 };
